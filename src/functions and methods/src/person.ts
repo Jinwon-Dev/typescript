cabac7020a80e9565639acc501f2e14e152d6ff0
Jinwon-Dev <yjw9424@naver.com>
2022-07-25 17:45:04 +0900

    함수 구현 기법/ 객체 생성 시 값 부분 생략

--- src/functions and methods/src/person.ts
+++ src/functions and methods/src/person.ts
@@ -7,11 +7,7 @@ type Person = {
 type MakePerson = (arg0: string, arg1: number, arg2?: boolean) => Person;
 
 export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person => {
-    const person: Person = {
-        name: name,
-        age: age,
-        etc: etc
-    };
+    const person: Person = {name, age, etc}; // 속성 이름과 매개 변수 이름이 같을 시 적용 가능
 
     return person;
 }
