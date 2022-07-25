1575da5d73518baa3d55a941233c3574c91a66ba
Jinwon-Dev <yjw9424@naver.com>
2022-07-25 19:01:45 +0900

    클래스 메서드

--- src/functions and methods/src/index.ts
+++ src/functions and methods/src/index.ts
@@ -1,12 +1,12 @@
 import {printHello} from "./print.hello";
 import {printUser} from "./print.user";
-import {makePerson, printPerson} from "./person";
+import {makePerson} from "./person";
 import {init} from "./init";
 
 printHello();
 printUser('Jinwon', 24);
 
 const person = makePerson('Jinwon', 24);
-printPerson(person);
+person.printInfo();
 
 init(() => console.log('custom initialization finished'));
--- src/functions and methods/src/person.ts
+++ src/functions and methods/src/person.ts
@@ -1,15 +1,19 @@
-type Person = {
-    name: string,
-    age: number,
-    etc?: boolean
-};
+class Person {
+    constructor(
+        private name: string,
+        private age: number,
+        private etc?: boolean
+    ) {}
 
-export const printPerson = ({name, age}: Person): void => {
-    console.log(`Person name: ${name}`);
-    console.log(`Person age: ${age}`);
+    printInfo: () => void = (): void => {
+        console.log(`Person name: ${this.name}`);
+        console.log(`Person age: ${this.age}`);
+        console.log(`Person etc: ${this.etc}`);
+    }
 }
 
 type MakePerson = (arg0: string, arg1: number, arg2?: boolean) => Person;
 
-export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person =>
-    ({name, age, etc});
+export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person => {
+    return new Person(name, age, etc);
+}
