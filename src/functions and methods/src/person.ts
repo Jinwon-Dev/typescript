9850f426a652a7082a0f4ec3e83ce0706df2b174
Jinwon-Dev <yjw9424@naver.com>
2022-07-25 17:49:21 +0900

    함수 구현 기법/ 객체 반환 화살표 함수

--- src/functions and methods/src/person.ts
+++ src/functions and methods/src/person.ts
@@ -6,8 +6,5 @@ type Person = {
 
 type MakePerson = (arg0: string, arg1: number, arg2?: boolean) => Person;
 
-export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person => {
-    const person: Person = {name, age, etc}; // 속성 이름과 매개 변수 이름이 같을 시 적용 가능
-
-    return person;
-}
+export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person =>
+    ({name, age, etc});
