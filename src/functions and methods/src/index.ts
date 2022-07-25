296922ffabc10f7377f73e5bf515727db0e5149b
Jinwon-Dev <yjw9424@naver.com>
2022-07-25 16:45:31 +0900

    함수 시그니처를 이해하기

--- src/functions and methods/src/index.ts
+++ src/functions and methods/src/index.ts
@@ -1,3 +1,5 @@
 import {printHello} from "./print.hello";
+import {printUser} from "./print.user";
 
 printHello();
+printUser('Jinwon', 24);
---
+++ src/functions and methods/src/print.user.ts
@@ -0,0 +1,5 @@
+export const printUser: (arg0: string, arg1: number) => void = function(name: string, age: number): void
+{
+    console.log(`User's name: ${name}`);
+    console.log(`User's age: ${age}`);
+};
